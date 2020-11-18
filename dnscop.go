99b9b95a8b5d81d90c7f1aae9478f4ed412ec764
Yasushi Ichikawa <ichikaway@gmail.com>
2020-11-18 10:25:49 +0900

    add block-list option

--- dnscop.go
+++ dnscop.go
@@ -17,12 +17,14 @@ func main() {
 	listen := flag.String("listen", ":53", "listen IP:Port. ex. 127.0.0.1:53")
 	resolver := flag.String("resolver", "8.8.8.8:53", "Resolver IP:Port. ex. 8.8.8.8:53")
 	between := flag.String("between", "20:00-23:00", "between time ex. 20:00-07:00")
+	blockList := flag.String("block-list", "www.youtube.com|youtube.com|i.ytimg.com|.+.googlevideo.com", "block list domains. ")
 	flag.Parse()
 
 	log.Printf("==== DNSCOP (ver %s) ====\n", VERSION)
 	log.Println("Listen: " + *listen)
 	log.Println("Resolver: " + *resolver)
 	log.Println("Between: " + *between)
+	log.Println("BlockList: " + *blockList)
 
 	packet, err := net.ListenPacket("udp", *listen)
 	if err != nil {
@@ -30,7 +32,7 @@ func main() {
 	}
 	defer packet.Close()
 
-	conf := config.NewUserConfig("www.youtube.com|youtube.com|i.ytimg.com|.+.googlevideo.com", *between)
+	conf := config.NewUserConfig(*blockList, *between)
 
 	for {
 		buf := make([]byte, maxDnsPacketSize)
@@ -50,7 +52,7 @@ func handleDnsRequest(packet net.PacketConn, address net.Addr, data []byte, reso
 	log.Println(name + " " + address.String())
 
 	if block.IsBlock(name, conf) {
-		log.Println("  ** block youtube **")
+		log.Println("  ** block: " + name + " **  " + address.String())
 		return
 	}
 
